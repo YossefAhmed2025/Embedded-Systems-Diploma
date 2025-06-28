714427980c18c647f60d64f67b2ea8bded290aa5
youssef <youssefahmed00022@gmail.com>
2025-06-28 17:17:36 +0300

    Assignement 1

---
+++ Assignement 1/main.c
@@ -0,0 +1,81 @@
+#include "stdio.h"
+//EX1
+/*void main(){
+	printf("concle output");
+}*/
+
+
+//EX2
+/*void main(){
+	 int x;
+	 printf("enter a integer");
+	 fflush(stdin); fflush(stdout);
+	 scanf("%d",&x);
+	 printf("your integer= %d",x);
+
+}*/
+
+//EX3
+
+/*int main(){
+	 int x;
+	 int y;
+	 int sum;
+	 printf("enter two integer");
+	 fflush(stdin); fflush(stdout);
+	 scanf("%d %d",&x,&y);
+	 sum=x+y;
+     printf("the sum= %d ",sum);
+}*/
+
+
+//EX4
+/*int main(){
+
+	     float x;
+		 float y;
+		 float sum;
+		 printf("enter two numbers");
+		 fflush(stdin); fflush(stdout);
+		 scanf("%f %f",&x,&y);
+		 sum=x*y;
+	     printf("the sum= %f ",sum);
+
+}*/
+
+
+//EX5
+/*int main(){
+
+	char c;
+	printf("enter chatacter: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&c);
+	printf("ASCII code is:%c= %d",c);
+
+}*/
+
+//EX6
+
+int main(){
+	int x;
+	int y;
+	int temp;
+	printf("enter two number want to swap: \n");
+	fflush(stdin);fflush(stdout);
+	scanf("%d %d",&x,&y);
+	printf("frist nummer before swap: %d",x);
+	printf("second nummer before swap: %d",y);
+	temp=x;
+	x=y;
+	y=temp;
+	printf("frist nummer After swap: %d",x);
+	printf("second nummer After swap: %d",y);
+
+
+}
+
+
+
+
+
